1f998116ffa96145f51837c004124d47e495d2f7
TatsuyaInagaki <engineering20240925@gmail.com>
2024-11-10 13:10:19 +0900

    inputのwidthを修正

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -26,7 +26,7 @@ const AddTodo = ({ handleAddTodo }: AddTodoProps) => {
             type="text"
             value={inputTitle}
             onChange={handleTitleChange}
-            className="font-mono w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-md px-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
+            className="font-mono w-80 bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-md px-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
             placeholder="Programming..."
           />
           <button
